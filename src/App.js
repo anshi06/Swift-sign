8678cf126f65d23798eeb65a6c4bcbc4b830953b
anshika-kc <anshika@kindtech.co.in>
2024-03-18 11:21:56 +0530

    use hashrouter

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import styles from "./styles/app.module.css";
 import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 import MainPage from "./components/MainPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.app}>
-      <BrowserRouter basename="/Swift-sign">
+      <Router basename="/Swift-sign">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/generate" element={<MainPage />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
       <Footer />
     </div>
   );
